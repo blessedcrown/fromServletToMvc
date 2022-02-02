01890dc85fb105522a87737b0c1b84624b0758be
blessedcrown <blessedcrown1@gmail.com>
2022-02-02 23:24:34 +0900

    V3의 구조에서 조금더 단순하고 사용자가 사용하기 쉬운 형태로 리팩토링

--- src/main/java/hello/servlet/web/frontcontroller/v4/ControllerV4.java
+++ src/main/java/hello/servlet/web/frontcontroller/v4/ControllerV4.java
@@ -2,6 +2,6 @@ package hello.servlet.web.frontcontroller.v4;
 
 import java.util.Map;
 
-public interface Controller4 {
+public interface ControllerV4 {
     String process(Map<String, String> paramMap, Map<String, Object> model);
 }
